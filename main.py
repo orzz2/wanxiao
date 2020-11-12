98bd82e6e534cb716ed9d0ce7539871bb34fd93a
orzz2 <62045367+orzz2@users.noreply.github.com>
2020-11-12 21:07:55 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import time,json,requests,random,datetime
+from campus import CampusCard
+
+def main():
+  #sectets字段录入
+  users = input()
+  
+  #数据检测
+  print(users) 
+  
+if __name__ == '__main__':
+    main()
